40e4406fa0df6f786b9a65d543eeda3e57dd1e6d
Bogdan <bogdan.plockiy@mail.ru>
2023-11-06 22:15:41 +0300

    градиентный спуск с дроблением шага готов

--- MatProgLW-2023/MatProgLW-2023.cpp
+++ MatProgLW-2023/MatProgLW-2023.cpp
@@ -1,20 +1,146 @@
-﻿// MatProgLW-2023.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+template<class Ty>
+struct vec3
+{
+	Ty x;
+	Ty y;
+	Ty z;
+
+	double len()
+	{
+		return(sqrt(x * x + y * y + z * z));
+	}
+
+	vec3<Ty> normalize()
+	{
+		return (len() > 0 ? *this / len() : vec3<Ty>{ 0,0,0 });
+	}
+
+	template<class Ty>
+	friend ostream& operator<<(ostream& out, const vec3<Ty>& vec)
+	{
+		return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+	}
+
+	template<class Ty>
+	friend vec3<Ty> operator-(const vec3<Ty>& vec, const vec3<Ty>& vec2)
+	{
+		return { vec.x - vec2.x, vec.y - vec2.y, vec.z - vec2.z };
+	}
+
+	template<class Ty>
+	friend vec3<Ty> operator+(const vec3<Ty>& vec, const vec3<Ty>& vec2)
+	{
+		return { vec2.x + vec.x, vec2.y + vec.y, vec2.z + vec.z };
+	}
+
+	template<class Ty>
+	friend vec3<Ty> operator*(const Ty& num, const vec3<Ty>& vec)
+	{
+		return { vec.x * num, vec.y * num, vec.z * num };
+	}
+
+	template<class Ty>
+	friend vec3<Ty> operator*(const vec3<Ty>& vec, const Ty& num)
+	{
+		return { vec.x * num, vec.y * num, vec.z * num };
+	}
+
+	template<class Ty>
+	friend vec3<Ty> operator/(const vec3<Ty>& vec, const Ty& num)
+	{
+		return { vec.x / num, vec.y / num, vec.z / num };
+	}
+
+	template<class Ty>
+	friend vec3<Ty> operator/(const Ty& num, const vec3<Ty>& vec)
+	{
+		return { num / vec.x, num / vec.y , num / vec.z };
+	}
+};
+
+using vec3d = vec3<double>;
+
+
+double func(vec3d vec)
+{
+	return (4 * vec.x * vec.x + 2 * vec.y * vec.y + 2 * vec.z * vec.z + vec.x * vec.z + 2 * vec.x * vec.y - 6 * vec.x - 8 * vec.z);
+}
+
+double der_x1(vec3d vec)
+{
+	return (func(vec + vec3d{ 1,0,0 }) - func(vec - vec3d{ 1,0,0 })) / 2;
+}
+
+double der_x2(vec3d vec)
+{
+	return (func(vec + vec3d{ 0,1,0 }) - func(vec - vec3d{ 0,1,0 })) / 2;
+}
+
+double der_x3(vec3d vec)
+{
+	return (func(vec + vec3d{ 0,0,1 }) - func(vec - vec3d{ 0,0,1 })) / 2;
+}
+
+vec3d grad(vec3d vec)
+{
+	return{ der_x1(vec), der_x2(vec), der_x3(vec) };
+}
+
+void grad_step(vec3d x0)
+{
+	double eps = 0.001;
+	int count = 0;
+	double step = 0.5;
+
+	double curf = func(x0);
+	double newf = 0;
+	vec3d newx = x0;
+
+	cout << " >> Градиентный спуск с дроблением шага:\n";
+	cout << setw(10) << left << "Итерация"
+		<< setw(10) << left << "x"
+		<< setw(10) << left << "y"
+		<< setw(10) << left << "z"
+		<< setw(16) << left << "||gradf(x)||"
+		<< setw(12) << left << "f(prev)"
+		<< setw(12) << left << "f(now)"
+		<< setw(12) << left << "step"
+		<< endl;
+
+	while (grad(x0).len() > eps)
+	{
+		x0 = newx;
+
+		curf = func(x0);
+
+		newx = x0 - step * grad(x0).normalize();
+
+		newf = func(newx);
+
+		if (curf < newf)step *= 0.5;
+
+		cout << setw(10) << left << ++count
+			<< setw(10) << left << setprecision(4)<<fixed << x0.x
+			<< setw(10) << left << setprecision(4)<<fixed << x0.y
+			<< setw(10) << left << setprecision(4)<<fixed << x0.z
+			<< setw(16) << left << defaultfloat << grad(x0).len()
+			<< setw(12) << left << curf
+			<< setw(12) << left << newf
+			<< setw(12) << left << step
+			<< endl;
+	}
+
+	cout << "\nПолученная точка экстремума: " << x0 << endl;
+	cout << "Количество итераций: " << count << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "RUSSIAN");
+	grad_step({ 0,0,0 });
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
