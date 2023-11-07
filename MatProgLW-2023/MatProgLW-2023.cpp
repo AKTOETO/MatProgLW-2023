58295ad5b10a8b8fae2b84af4cf65030541fd1db
Bogdan <bogdan.plockiy@mail.ru>
2023-11-07 22:13:22 +0300

    переделал расчет шага и расчет производных

--- MatProgLW-2023/MatProgLW-2023.cpp
+++ MatProgLW-2023/MatProgLW-2023.cpp
@@ -67,68 +67,88 @@ using vec3d = vec3<double>;
 
 double func(vec3d vec)
 {
-	return (4 * vec.x * vec.x + 2 * vec.y * vec.y + 2 * vec.z * vec.z + vec.x * vec.z + 2 * vec.x * vec.y - 6 * vec.x - 8 * vec.z);
+	return (
+		4 * vec.x * vec.x + 2 * vec.y * vec.y + 2 * vec.z * vec.z +
+		vec.x * vec.z + 2 * vec.x * vec.y - 6 * vec.x - 8 * vec.z
+		);
 }
 
+// производная по первой переменной
 double der_x1(vec3d vec)
 {
-	return (func(vec + vec3d{ 1,0,0 }) - func(vec - vec3d{ 1,0,0 })) / 2;
+	return 8 * vec.x + vec.z + 2 * vec.y - 6;
+	//return (func(vec + vec3d{ 1,0,0 }) - func(vec - vec3d{ 1,0,0 })) / 2;
 }
 
+// производная по второй переменной
 double der_x2(vec3d vec)
 {
-	return (func(vec + vec3d{ 0,1,0 }) - func(vec - vec3d{ 0,1,0 })) / 2;
+	return 4 * vec.y + 2 * vec.x;
+	//return (func(vec + vec3d{ 0,1,0 }) - func(vec - vec3d{ 0,1,0 })) / 2;
 }
 
+// производная по третей переменной
 double der_x3(vec3d vec)
 {
-	return (func(vec + vec3d{ 0,0,1 }) - func(vec - vec3d{ 0,0,1 })) / 2;
+	return 4 * vec.z + vec.x - 8;
+	//return (func(vec + vec3d{ 0,0,1 }) - func(vec - vec3d{ 0,0,1 })) / 2;
 }
 
+// расчет вектора градиента
 vec3d grad(vec3d vec)
 {
 	return{ der_x1(vec), der_x2(vec), der_x3(vec) };
 }
 
-void grad_step(vec3d x0)
+// печать шапки таблицы
+void print_table_header()
 {
-	double eps = 0.001;
-	int count = 0;
-	double step = 0.5;
-
-	double curf = func(x0);
-	double newf = 0;
-
 	cout << " >> Градиентный спуск с дроблением шага:\n";
 	cout << setw(10) << left << "Итерация"
 		<< setw(10) << left << "x"
 		<< setw(10) << left << "y"
 		<< setw(10) << left << "z"
 		<< setw(16) << left << "||gradf(x)||"
-		<< setw(12) << left << "f(prev)"
-		<< setw(12) << left << "f(now)"
 		<< setw(12) << left << "step"
 		<< endl;
+}
+
+// печать строки таблицы
+void print_table_string(
+	int count,
+	vec3d x0,
+	double step
+)
+{
+	cout << setw(10) << left << ++count
+		<< setw(10) << left << setprecision(4) << fixed << x0.x
+		<< setw(10) << left << setprecision(4) << fixed << x0.y
+		<< setw(10) << left << setprecision(4) << fixed << x0.z
+		<< setw(16) << left << defaultfloat << grad(x0).len()
+		<< setw(12) << left << step
+		<< endl;
+}
+
+void find_extr(vec3d x0)
+{
+	vec3d x1;
+
+	double eps = 0.001;
+	int count = 0;
+	double step = 1;
+
+	print_table_header();
 
 	while (grad(x0).len() > eps)
 	{
-		curf = func(x0);
+		x1 = x0 - step * grad(x0);
 
-		x0 = x0 - step * grad(x0).normalize();
+		while (func(x0) <= func(x1))
+			x1 = x0 - (step /= 2) * grad(x0);
 
-		newf = func(x0);
+		x0 = x1;
 
-		if (curf < newf)step *= 0.5;
-
-		cout << setw(10) << left << ++count
-			<< setw(10) << left << setprecision(4)<<fixed << x0.x
-			<< setw(10) << left << setprecision(4)<<fixed << x0.y
-			<< setw(10) << left << setprecision(4)<<fixed << x0.z
-			<< setw(16) << left << defaultfloat << grad(x0).len()
-			<< setw(12) << left << curf
-			<< setw(12) << left << newf
-			<< setw(12) << left << step
-			<< endl;
+		print_table_string(count++, x0, step);
 	}
 
 	cout << "\nПолученная точка экстремума: " << x0 << endl;
@@ -138,5 +158,5 @@ void grad_step(vec3d x0)
 int main()
 {
 	setlocale(LC_ALL, "RUSSIAN");
-	grad_step({ 0,0,0 });
+	find_extr({ 0,0,0 });
 }
