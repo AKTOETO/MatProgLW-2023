30489cc2692363ff9e17c48760f7b3fc5fa5dd4e
Bogdan <bogdan.plockiy@mail.ru>
2023-11-22 20:11:55 +0300

    град спуск с оптимиз. по шагу для юры готов

--- MatProgLW-2023/MatProgLW-2023.cpp
+++ MatProgLW-2023/MatProgLW-2023.cpp
@@ -68,30 +68,30 @@ using vec3d = vec3<double>;
 double func(vec3d vec)
 {
 	return (
-		4 * vec.x * vec.x + 2 * vec.y * vec.y + 2 * vec.z * vec.z +
-		vec.x * vec.z + 2 * vec.x * vec.y - 6 * vec.x - 8 * vec.z
+		-3 * vec.x * vec.x - 2 * vec.y * vec.y + vec.x * vec.y +
+		5 * vec.x - vec.z * vec.z - vec.y * vec.z + 6 * vec.z
 		);
 }
 
 // производная по первой переменной
 double der_x1(vec3d vec)
 {
-	return 8 * vec.x + vec.z + 2 * vec.y - 6;
-	//return (func(vec + vec3d{ 1,0,0 }) - func(vec - vec3d{ 1,0,0 })) / 2;
+	// return 8 * vec.x + vec.z + 2 * vec.y - 6;
+	return (func(vec + vec3d{ 1,0,0 }) - func(vec - vec3d{ 1,0,0 })) / 2;
 }
 
 // производная по второй переменной
 double der_x2(vec3d vec)
 {
-	return 4 * vec.y + 2 * vec.x;
-	//return (func(vec + vec3d{ 0,1,0 }) - func(vec - vec3d{ 0,1,0 })) / 2;
+	// return 4 * vec.y + 2 * vec.x;
+	return (func(vec + vec3d{ 0,1,0 }) - func(vec - vec3d{ 0,1,0 })) / 2;
 }
 
 // производная по третей переменной
 double der_x3(vec3d vec)
 {
-	return 4 * vec.z + vec.x - 8;
-	//return (func(vec + vec3d{ 0,0,1 }) - func(vec - vec3d{ 0,0,1 })) / 2;
+	// return 4 * vec.z + vec.x - 8;
+	return (func(vec + vec3d{ 0,0,1 }) - func(vec - vec3d{ 0,0,1 })) / 2;
 }
 
 // расчет вектора градиента
@@ -133,22 +133,22 @@ void print_table_string(
 double argmin_f(const vec3d& vec)
 {
 	// антиградиент функции
-	auto anti_gr = grad(vec).normalize();
+	auto anti_gr = (-1.)*grad(vec).normalize();
 
 	// в f подставляем x-t*grad заместо x f(x-t*grad) 
 	// и упрощаем полученное выражение.
 	// результат дифферинцируем по t, приравниваем к 0, 
 	// выводим t(типо так: t = ..., t будет равна дроби)
 
-	double numerator = 
-		8 * vec.x * anti_gr.x + 4 * vec.y * anti_gr.y + 4 * vec.z * anti_gr.z +
-		vec.x * anti_gr.z + vec.z * anti_gr.x + 2 * vec.x * anti_gr.y + 
-		2 * vec.y * anti_gr.x - 6 * anti_gr.x - 8 * anti_gr.z;
+	double numerator =
+		6 * vec.x * anti_gr.x + 4 * vec.y * anti_gr.y - vec.x * anti_gr.y - 
+		anti_gr.x * vec.y - 5 * anti_gr.x + 2 * vec.z * anti_gr.z + 
+		vec.y * anti_gr.z + vec.z * anti_gr.y - 6 * anti_gr.z;
 
-	double denumerator = 
-		8 * anti_gr.x * anti_gr.x + 4 * anti_gr.y * anti_gr.y +
-		4 * anti_gr.z * anti_gr.z + 2 * anti_gr.x * anti_gr.z +
-		4 * anti_gr.x * anti_gr.y;
+	double denumerator =
+		-6 * anti_gr.x * anti_gr.x - 4 * anti_gr.y * anti_gr.y +
+		2 * anti_gr.x * anti_gr.y - 2 * anti_gr.z * anti_gr.z +
+		2 * anti_gr.x * anti_gr.z;
 
 	return numerator / denumerator;
 }
